c00ec85db4842b2c948238afd87138ea2892f5c0
TOLSTbIU88 <gamer-9600@yandex.ru>
2022-11-15 00:00:30 +0300

    start

---
+++ src/main/java/org/example/HibernateConfig.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateConfig {
+    public static SessionFactory createSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+}
