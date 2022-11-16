e7134043c56b3fe1365201060703535a9377a968
TOLSTbIU88 <gamer-9600@yandex.ru>
2022-11-16 22:59:35 +0300

    >>

--- src/main/java/org/example/Runner.java
+++ src/main/java/org/example/Runner.java
@@ -10,6 +10,21 @@ public class Runner {
         try (SessionFactory sessionFactory = HibernateConfig.createSessionFactory();
              Session session = sessionFactory.openSession();) {
             session.beginTransaction();
+            User Nikolay = new User("Nikolay","234");
+            User Vasilii = new User("Vasilii","23434");
+            Post post = new Post("Предлогаю выпить",Nikolay);
+            Post post2 = new Post("Заколебали бухать",Vasilii);
+            Comment comment = new Comment("Не вижу повода не выпить",post,Nikolay);
+            Comment comment2 = new Comment("Не согласен",post2,Vasilii);
+            session.saveOrUpdate(Nikolay);
+            session.saveOrUpdate(Vasilii);
+            session.saveOrUpdate(post);
+            session.saveOrUpdate(post2);
+            session.saveOrUpdate(comment);
+            session.saveOrUpdate(comment2);
+            session.getTransaction().commit();
+            session.clear();
+
         }
     }
 }
