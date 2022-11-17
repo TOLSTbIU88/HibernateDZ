a6543bb210547618b97c02e4bf21b56c96fd3268
TOLSTbIU88 <gamer-9600@yandex.ru>
2022-11-17 22:28:59 +0300

    >>

--- src/main/java/org/example/InstDao.java
+++ src/main/java/org/example/InstDao.java
@@ -4,9 +4,6 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class InstDao {
-    public User findUser(int id) {
-        return Runner.getSessionFactory().openSession().get(User.class, id);
-    }
     public void addUSER(User user) {
         Session session = Runner.getSessionFactory().openSession();
         Transaction start = session.beginTransaction();
